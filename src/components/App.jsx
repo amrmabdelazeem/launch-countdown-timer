19194fd7ba2df08e70662089c34ad3ebad67de70
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-08 20:50:05 +0300

    Create structure for the project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function App(){
-    return <h1>react</h1>
+    return <>
+        <header role="banner">
+            <h1>WE'RE LAUNCHING SOON</h1>
+        </header>
+        <main role="main">
+            <div className="card">
+                <div className="page1"></div>
+                <div className="page2">
+                    <h2></h2>
+                </div>
+            </div>
+        </main>
+        <footer role="contentinfo"></footer>
+    </>
 }
