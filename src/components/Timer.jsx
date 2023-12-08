518680d59a3044b4e5860a9217f137f38b798ed8
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-09 01:56:09 +0300

    Add hover states for social media icons

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Timer({time}) {
   return (
       <div className="card">
-      <div className="page1"><h2>38</h2></div>
+      <div className="page1"><h2>48</h2></div>
       <div className="page2"><p>{time}</p></div>
       </div>
   );
