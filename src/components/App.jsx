dda542b72dcb4b2e46443421c7e8bcc19e3449a8
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-08 21:32:00 +0300

    Add new component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import Timer from "./Timer";
 
 export default function App(){
     return <>
         <header role="banner">
             <h1>WE'RE LAUNCHING SOON</h1>
         </header>
-        <main role="main">
-            <div className="card">
-                <div className="page1">
-                {/* <h2>day</h2> */}
-                </div>
-                <div className="page2">
-                </div>
-            </div>
+        <main role="main" className="timer">
+        <Timer/>
+        <Timer/>
+        <Timer/>
+        <Timer/>
         </main>
         <footer role="contentinfo"></footer>
     </>
---
+++ src/components/Timer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Timer() {
+  return (
+      <div className="card">
+      <div className="page1"></div>
+      <div className="page2"></div>
+      </div>
+  );
+}
