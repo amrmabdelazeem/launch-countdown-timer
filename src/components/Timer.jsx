089a55b7a0ccd26560e273f1d3539a9fb6b8de9e
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-09 17:16:40 +0300

    Passing props to output

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Timer({time}) {
+export default function Timer({time, show}) {
   
   return (
       <div className="card">
-      <div className="page1"><h2>48</h2></div>
+      <div className="page1"><h2>{show}</h2></div>
       <div className="page2"><p>{time}</p></div>
       </div>
   );
