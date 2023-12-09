d0ba7f059cab0e8afe12249decc00ae278928cba
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-09 16:10:49 +0300

    Get date and use maths to calculate time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const newDate = new Date("Dec 24, 2023 00:00:00").getTime();
+const now = new Date().getTime();
+const timeRemaining = newDate - now;
+
+const days = Math.floor(timeRemaining / (1000 *60 * 60 * 24));
+const hours = Math.floor((timeRemaining % (1000 *60*60*24)/ (1000 * 60 * 60)));
+const mintues = Math.floor((timeRemaining % (1000*60*60)/ (1000* 60)));
+const seconds = Math.floor((timeRemaining % (1000* 60)/ 1000));
+
+
+
 root.render(
   <React.StrictMode>
     <App />
