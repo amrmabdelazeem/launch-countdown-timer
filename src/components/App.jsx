5aac549c82a4ab41726913a2df2ddc2a5db2f043
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-09 17:17:19 +0300

    Create state for the timer and logic formulas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Timer from "./Timer";
 
-export default function App(){
+export default function App() {
+  const [newTime, setNewTime] = useState({
+    days: "",
+    hours: "",
+    minutes: "",
+    seconds: "",
+  });
 
+  function handleTime() {
+    setNewTime((preSecond) => {
+      return { ...preSecond, days, hours, minutes, seconds };
+    });
+  }
+  const newDate = new Date("Dec 24, 2023 00:00:00").getTime();
+  const now = new Date().getTime();
+  const timeRemaining = newDate - now;
 
-    return <>
-        <header role="banner" className="title">
-            <h1>WE'RE LAUNCHING SOON</h1>
-        </header>
-        <main role="main" className="timer">
-        <Timer time="DAYS"/>
-        <Timer time="HOURS"/>
-        <Timer time="MINUTES"/>
-        <Timer time="SECONDS"/>
-        </main>
-        <footer role="contentinfo">
-        <img src="./images/icon-facebook.svg" alt="facebook-icon"/>
-        <img src="./images/icon-pinterest.svg" alt="pinterest-icon"/>
-        <img src="./images/icon-instagram.svg" alt="instagram-icon"/>
-        </footer>
+  const days = String(Math.floor(timeRemaining / (1000 * 60 * 60 * 24)));
+  const hours = String(
+    Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const minutes = String(
+    Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60))
+  );
+  const seconds = String(Math.floor((timeRemaining % (1000 * 60)) / 1000));
+
+  return (
+    <>
+      <header role="banner" className="title">
+        <h1>WE'RE LAUNCHING SOON</h1>
+      </header>
+      <main role="main" className="timer" onClick={handleTime}>
+        <Timer time="DAYS" show={newTime.days} />
+        <Timer time="HOURS" show={newTime.hours} />
+        <Timer time="MINUTES" show={newTime.minutes} />
+        <Timer time="SECONDS" show={newTime.seconds} />
+      </main>
+      <footer role="contentinfo">
+        <img src="./images/icon-facebook.svg" alt="facebook-icon" />
+        <img src="./images/icon-pinterest.svg" alt="pinterest-icon" />
+        <img src="./images/icon-instagram.svg" alt="instagram-icon" />
+      </footer>
     </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const newDate = new Date("Dec 24, 2023 00:00:00").getTime();
-const now = new Date().getTime();
-const timeRemaining = newDate - now;
 
-const days = Math.floor(timeRemaining / (1000 *60 * 60 * 24));
-const hours = Math.floor((timeRemaining % (1000 *60*60*24)/ (1000 * 60 * 60)));
-const mintues = Math.floor((timeRemaining % (1000*60*60)/ (1000* 60)));
-const seconds = Math.floor((timeRemaining % (1000* 60)/ 1000));
 
 
 
